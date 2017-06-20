650df5fb8119b572cecd39edd522d6c878dca956
Roychenlei <roychenfly@gmail.com>
2017-06-20 17:16:54 +0800

    new edit

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,3 +1,36 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+def post_create(request):
+	return HttpResponse("<h1>Create</h1>")
+
+
+def post_detail(request):
+
+	context ={
+			"title":"Detail"
+	}
+	return render(request,"index.html",context)
+
+
+def post_list(request):
+	if request.user.is_authenticated():
+		context ={
+			"title":"My user List"
+		}
+	else:
+		context ={
+			"title":"List"
+		}
+
+	return render(request,"index.html",context)
+
+
+def post_update(request):
+	return HttpResponse("<h>Update</h>")
+
+
+def post_delete(request):
+	return HttpResponse("<h>Delete</h>")

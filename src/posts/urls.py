318d80800ef1807456e3e39002293d1ba390ff7c
Roychenlei <roychenfly@gmail.com>
2017-07-07 18:49:50 +0800

    crud

--- src/posts/urls.py
+++ src/posts/urls.py
@@ -1,12 +1,24 @@
 from django.conf.urls import url
 
+from django.contrib import admin
+
 from . import views
 
 
-app_name = 'posts'
+# app_name = 'posts'
+from .views import (
+	post_list,
+	post_create,
+	post_detail,
+	post_update,
+	post_delete,
+	)
+
 urlpatterns = [
-	url(r'^$',views.index,name='index'),
-	url(r'^(?P<post_id>[0-9]+)$',views.detail,name='detail'),
-	url(r'^(?P<post_id>[0-9]+)/results/$',views.results,name='result'),
-	url(r'^(?P<post_id>[0-9]+)/vote/$',views.vote,name='vote'),
-	]
+	url(r'^$', post_list,name='list'),
+    url(r'^create/$', post_create),
+    url(r'^(?P<id>\d+)/$', post_detail,name='detail'),
+    url(r'^(?P<id>\d+)/edit/$', post_update,name='update'),
+    url(r'^(?P<id>\d+)/delete/$', post_delete),
+    #url(r'^posts/$', "<appname>.views.<function_name>"),
+]
--- src/posts/views.py
+++ src/posts/views.py
@@ -1,25 +1,80 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render,get_object_or_404,redirect
 from .models  import Post
 from django.http import Http404
+from django.http import HttpResponse,HttpResponseRedirect
+from .forms import PostForm
+from django.contrib import messages
 
 
+def post_create(request):
+	form = PostForm(request.POST or None)
+	if form.is_valid():
+		instance=form.save(commit=False)
+		print (form.cleaned_data.get("title"))
+		print (form.cleaned_data.get("content"))
 
-def index(request):
-	latest_list = Post.objects.order_by('updated')[:5]
+		instance.save()
+		messages.success(request, "Successfully Created")
+		return HttpResponseRedirect(instance.get_absolute_url())
+	else:
+		messages.error(request,"Not successfully created")
 	context={
-	'latest_list': latest_list
+		"form":form,
 	}
-	return render(request,'posts/index.html',context)
-
-def detail(request,post_id):
-	post = get_object_or_404(Post,pk=post_id)
-	return render(request,'posts/detail.html',{'post':post})
 
 
-def results(request,post_id):
-	return HttpResponse("posts %s" % post_id)
 
-def vote(request,post_id):
-	return HttpResponse("posts %s" % post_id)
+	return render(request,"post_form.html",context)
+
+
+
+
+
+def post_detail(request,id=None): #retrieve
+
+	instance = Post.objects.get(id=id)
+
+	context = {
+		"title": instance.title,
+		"instance":instance,
+	}
+	return render(request, "post_detail.html", context)
+
+
+
+def post_list(request): #list items
+
+	queryset = Post.objects.all()
+	
+	context={
+			"object_list": queryset,
+			"title":"List"
+	}
+
+
+	return render(request,"post_list.html",context)
+
+def post_update(request,id=None):
+
+	instance = get_object_or_404(Post,id=id)
+	form = PostForm(request.POST or None, instance=instance)
+	if form.is_valid():
+		instance = form.save(commit=False)
+		instance.save()
+		messages.success(request, "<a href='#'>Item</a> Saved", extra_tags='html_safe')
+		return HttpResponseRedirect(instance.get_absolute_url())
+	context={
+		"title":instance.title,
+		"instance":instance,
+		"form":form,
+	}
+	return render(request,"post_form.html",context)
+
+
+def post_delete(request,id=None):
+	instance = get_object_or_404(Post, id=id)
+	instance.delete()
+	messages.success(request, "Successfully deleted")
+	return redirect("posts:list")
 
 
